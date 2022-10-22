db23d927d51d7dfae1448a137183bcaba5c46662
Dmitry G <sdprecovery@gmail.com>
2022-10-22 17:06:12 +0300

    rename: app => main; create: consumer.py/producer.py for admin/main

---
+++ admin/products/producer.py
@@ -0,0 +1,11 @@
+import pika
+
+params = pika.URLParameters(
+    "amqps://gxttwwrl:GpfmS2JwgksYZCxQFlvD8rpUvXyo6YN_@cow.rmq2.cloudamqp.com/gxttwwrl"
+)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+
+def publish():
+    channel.basic_publish(exchange="", routing_key="main", body="hello")
