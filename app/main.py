e808303c31a7105a57c48e6cc62c673c73d5c1c2
Dmitry G <sdprecovery@gmail.com>
2022-10-22 19:03:15 +0300

    rename: main => app; update: admin/app

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
+from dataclasses import dataclass
+
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
-app.config["SQLAlchemy_DATABASE_URI"] = "mysql://root:root@db/main"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@db/main"
 CORS(app)
 
 db = SQLAlchemy(app)
 
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
--- app/manager.py
+++ app/manager.py
@@ -1,6 +1,5 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-
 from main import app, db
 
 migrate = Migrate(app, db)
